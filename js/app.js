84b74303aac908a80513563c08d8bd65340009ab
BJ Hayes <b.justinhayes@gmail.com>
2018-06-14 00:24:40 -0700

    went back and tried to make this work again, having problems with the last array

--- js/app.js
+++ js/app.js
@@ -2,96 +2,88 @@
 
 var pointsTracker = 0;
 
-// Question 1
-function abductionGame () {
-  alert ('Please answer the following questions with Yes or No answers');
+alert ('Please answer the following questions with Yes or No answers');
 
+function quizGame () {
+
+  // Question 1
   var abduction = prompt('Have I ever been abducted by aliens?').toLowerCase();
-
   if (abduction === 'yes') {
     alert('If it happened, I have no memory of it. I was once stranded on the side of Mount Shasta for 3 days which is known for it\'s high level of UFO activity...');
     pointsTracker = pointsTracker + 1;
-  } else if(abduction === 'no') {
-    alert('Not yet. But I want to believe...');
   }
-  else {
-    alert('You didn\'t answer yes or no');
-  }
-  console.log('Their answer is', abduction + ' to the alien question')
-}
-abductionGame();
 
-// Question 2
-function skaterGame() {
+  if(abduction === 'no') {
+    alert('Not yet. But I want to believe...');
+    pointsTracker - 1;
+  }
+
+  console.log('Their answer is', abduction + ' to the alien question');
+
   var skater = prompt('Do you think I can skateboard?').toLowerCase();
   if (skater === 'yes') {
     alert('Actually, I\'m terrible at it. But I *do* know how to snowboard.');
-    pointsTracker = pointsTracker + 1;
+    pointsTracker = pointsTracker -1 ;
   }
-  else {
-    alert('You are correct.')
-  }
-  console.log('Can I skateboard?',skater)
-}
-skaterGame();
 
-// Question 3
-function glutenGame () {
+  if (skater === 'no') {
+    alert('You are correct.');
+    pointsTracker +1;
+  }
+
+  console.log('Can I skateboard?',skater);
+
   var glutens = prompt('Am I gluten free?').toLowerCase();
   if (glutens === 'yes') {
     alert('No way, I love pizza waaaay too much');
-    pointsTracker = pointsTracker + 1;
+    pointsTracker = pointsTracker -1;
   }
-  else {
+  if (glutens === 'no') {
     alert('Bread is delicious. It makes you fat. But it is delicious.');
+    pointsTracker +1;
   }
-  console.log('Am I good with the glutens?',glutens)
-}
-glutenGame();
+  console.log('Am I good with the glutens?',glutens);
 
-// Question 4
-function lidGame () {
   var lid = prompt('Do I often leave my lid up?').toLowerCase();
   if (lid === 'no') {
     alert('They\'ll steal my secrets!');
-    pointsTracker = pointsTracker + 1;
+    pointsTracker + 1;
   }
-  else {
+
+  if (lid === 'yes') {
     alert('Hey, I\'m a trusting type of guy');
-  }
-  console.log('My lid discipline is on point',lid)
-}
-lidGame();
+    pointsTracker -1;
 
-// Question 5
-function pulpFictionGame() {
-  var pulpFiction = prompt('Have I seen Pulp Fiction so much that I can recite Samuel Jackson\'s Bible quote from memory?').toLowerCase();
-  if (pulpFiction === 'yes') {
-    alert('Ezekiel 25:17? Ezekiel 25:17. "The path of the righteous man is beset on all sides by the inequities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of the darkness. For he is truly his brother\'s keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy my brothers. And you will know I am the Lord when I lay my vengeance upon you.');
-    pointsTracker = pointsTracker + 1;
-  }
-  else {
-    alert('Actually, I can.')
-  }
-  console.log('Do they think I\'ve seen Pulp Fiction waaaay too many times?',pulpFiction)
-}
-pulpFictionGame();
+    console.log('My lid discipline is on point',lid);
 
-// this next section satisfies the "numbered guess" project requirement
-// the number of guinea pigs I've owned
-// Prompt for user to make a guess
+    var pulpFiction = prompt('Have I seen Pulp Fiction so much that I can recite Samuel Jackson\'s Bible quote from memory?').toLowerCase();
+    if (pulpFiction === 'yes') {
+      alert('Ezekiel 25:17? Ezekiel 25:17. "The path of the righteous man is beset on all sides by  the inequities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of the darkness. For he is truly his brother\'s keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy my brothers. And you will know I am the Lord when I lay my vengeance upon you.');
+      pointsTracker = pointsTracker + 2;
+      alert('You get a bonus point for that one!');
+    }
+
+    if (pulpFiction === 'no') {
+      alert('Actually, I can.');
+      pointsTracker -1;
+    }
+    console.log('Do they think I\'ve seen Pulp Fiction waaaay too many times?',pulpFiction);
+  }
+}
+quizGame();
 
 function pigsGame () {
   var totalGuesses = 0;
-  var howManyPigs;
+  var howManyPigs = 0;
   var pigNumber = 5;
+
   for(var i = 0; i < 4; i++) {
     howManyPigs = parseInt(prompt('You have 4 guesses to find out how many Guinea Pigs I\'ve owned.'));
     totalGuesses++;
     console.log('user has made ' + totalGuesses + 'guesses');
     if(howManyPigs === pigNumber) {
       alert('Exactly right! You must be psychic.');
-      pointsTracker = pointsTracker + 1;
+      pointsTracker++;
       break;
     }
     if (howManyPigs < pigNumber) {
@@ -110,41 +102,22 @@ function pigsGame () {
 }
 pigsGame();
 
-// this next section addresses the "guess my car in an array" project requirement
-//array that holds the cars I've owned
 
-function carsGame () {
+var correctGuess = false;
+var carTracker = 0;
+
+do {
   var ownedCars = ['honda', 'toyota', 'chevy', 'subaru'];
-  var cGuessesTrack = 0;
-
-  for(var i = 0; i < 6; i++) {
-    var carGuess = prompt('Can you guess the make (not model) of a car I\'ve owned?').toLowerCase();    
-      console.log('i is referring to:', ownedCars);
-
-  // prompt the user to guess the make of a car I've owned
-  // compares what the user entered to all of the elements in the array
-    if(ownedCars.includes(carGuess)) {
-      // indicates a correct guess
-      alert('Wow, nice guess!');
-      pointsTracker = pointsTracker + 1;
-      break;
-    } else {
-      cGuessesTrack++
-      console.log(cGuessesTrack);
-      continue;
-      if (cGuessesTrack = 6) {
-        alert('You ran out of guesses!');
-        break;
- // when you answer wrong the prompts go nope try again -> you ran out of guesses -> and then back to the prompt to try again
-      }
-    }
+  var carGuess = prompt('Can you guess the make (not model) of a car I\'ve owned?').toLowerCase();
+  if(carGuess !== (ownedCars.includes(carGuess)) {
+    correctGuess = false;
+    carTracker++;
   }
-}
-carsGame();
+  if(carGuess === ownedCars.includes(carGuess)) {
+    alert('Wow, nice guess!');
+    pointsTracker++;
+    break;
+  } while (carTracker > 6);
+};
 
-
-function totalPoints () {
-  alert('You have finished and you received ' + pointsTracker + ' points');
-}
-
-totalPoints();
+// alert('You scored ' + pointsTracker + ' out of 6 possible points!');
